66f95d0cf4b98aecd0bdaa2d37f5830c6580b3d0
Hideo Hattori <hattori-h@klab.com>
2016-07-11 21:48:41 +0900

    change standalone

--- rlogger_test.go
+++ rlogger_test.go
@@ -1,33 +1,67 @@
 package rlogger
 
 import (
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
-const SOCKET_PATH string = "/usr/local/var/run/rlogd/rloggerd.sock"
+var SOCKET_PATH string
 
 func TestSimpleWrite(t *testing.T) {
-	r := NewRLogger([]byte("example.acc.go"), SOCKET_PATH)
+	tag := []byte("this.is.tag")
+	msg := []byte("hogehoge1")
+	t.Logf("path=%v\n", SOCKET_PATH)
+	r := NewRLogger(SOCKET_PATH)
 	defer r.Close()
-	ret, err := r.Write([]byte("hogehoge1"))
+	ret, err := r.Write(tag, msg)
 	if err != nil {
 		t.Errorf("write error: err=%v", err)
 	}
 
-	if ret != 43 {
+	if ret != 40 {
 		t.Errorf("invalid msg len. len=%v", ret)
 	}
 }
 
 func TestTwoLineWrite(t *testing.T) {
-	r := NewRLogger([]byte("example.acc.go"), SOCKET_PATH)
+	tag := []byte("this.is.tag")
+	msg := []byte("hogehoge2\nhogehoge3")
+	r := NewRLogger(SOCKET_PATH)
 	defer r.Close()
-	ret, err := r.Write([]byte("hogehoge2\nhogehoge3"))
+	ret, err := r.Write(tag, msg)
 	if err != nil {
 		t.Errorf("write error: err=%v", err)
 	}
 
-	if ret != 60 {
+	if ret != 57 {
 		t.Errorf("invalid msg len. len=%v", ret)
 	}
+}
+
+func TestMain(m *testing.M) {
+	dir, err := ioutil.TempDir("", "gorlogger")
+	if err != nil {
+		panic(err)
+	}
+
+	SOCKET_PATH = filepath.Join(dir, "rloggerd.dummy.sock")
+	unixListener, err := net.ListenUnix("unix", &net.UnixAddr{Name: SOCKET_PATH, Net: "unix"})
+	if err != nil {
+		os.RemoveAll(dir)
+		panic(err)
+	}
+
+	go func() {
+		for {
+			unixListener.Accept()
+		}
+	}()
+
+	ret := m.Run()
+	os.RemoveAll(dir)
+
+	os.Exit(ret)
 }
